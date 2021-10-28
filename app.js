a3d99e6a92d73fbd27166c923d9aa8e854030b83
Luigi Tamagnini <bosca.music@gmail.com>
2021-10-28 16:04:20 -0300

    update_most_validations_solved

--- app.js
+++ app.js
@@ -22,6 +22,8 @@ window.onload = function() {
     function runEvent(e) {
         // console.log(e.target.id)
         switch (e.target.id) {
+
+            // full name validation
             case 'full-name':
                 const fName = e.target.value
                 const warning = document.createElement('small')
@@ -32,8 +34,7 @@ window.onload = function() {
                 if(fName.length<6) {
                     warning.textContent='your name must be at least 6 letters long'
                     console.log(warning)
-                }
-
+                } 
                 if(fName.search(' ') < 1) {
                     warning.textContent='your must enter your name and last name'
                     console.log(warning)
@@ -41,29 +42,73 @@ window.onload = function() {
                 const container = document.querySelector('.suscription-full-name') 
                 console.log(container)
                 container.parentNode.insertBefore(warning,container.nextSibling)
+            break;
 
-                break;
-                case 'email':
-                    console.log(e.target.value)
-                    const email = e.target.value
-                break;
+            // email validation 
+            case 'email':
+                const email = e.target.value
+                var regexp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+                if(regexp.test(email)==false) {
+                    console.log('not-valid')
+                }
+            break;
+
+            // age validation
             case 'age':
-                console.log(e.target.value)
+                // age validation
+                let age = e.target.value
+                age = parseInt(age,10)
+                if (Number.isInteger(age)&&age<18 || typeof(age) === NaN) {
+                    console.log('not-valid')
+                }
                 break;
-            case 'phone':
-                console.log(e.target.value)
+                
+                // pgone number validation
+                case 'phone':
+                    // validate phone number
+                    const phone = e.target.value
+                    if(phone.length < 7) {
+                        console.log('not-valid')
+                    }              
+                    // regexp
+                    var regexp = /[(|)| |-]/g;
+                    var myArray = regexp.exec(phone);
+                    if(regexp.lastIndex > 0) {
+                        console.log('not-valid') 
+                    }
                 break;
-            case 'address':
-                console.log(e.target.value)
+                
+                // address validation
+                case 'address':
+                    const address = e.target.value
+                    var regexp = /^[A-z0-9\s\.,/-]+$/
+                    if(regexp.test(address)==false || address.search(' ') < 1) {
+                        console.log('not-valid')
+                    }
                 break;
-            case 'city':
-                console.log(e.target.value)
+                
+                // city validation
+                case 'city':
+                    const city = e.target.value
+                    if(city.length < 3) {
+                        console.log('not-valid')
+                    }               
                 break;
-            case 'postal-code':
-                console.log(e.target.value)
+
+                // postal code validation 
+                case 'postal-code':
+                    const postalCode = e.target.value
+                    if(postalCode.length < 3) {
+                        console.log('not-valid')
+                    }
                 break;
-            case 'ID-number':
-                console.log(e.target.value)
+
+                // id number validation 
+                case 'ID-number':
+                    const id = e.target.value
+                    if(id.length < 7 || id.length > 8) {
+                        console.log('not-valid')
+                    }
                 break;
                 
             default:
