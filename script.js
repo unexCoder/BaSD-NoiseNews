44f8bfa7c41a8a367be83359d77164becdca8fb1
Luigi Tamagnini <bosca.music@gmail.com>
2021-11-03 09:36:34 -0300

    modal_mobile_navbar_implemented2

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 window.onload = function() {
 
-    //
+    // modal mobile navbar 
     var cross = document.querySelector('.cross-icon')
     cross.addEventListener('click',function(){
         cross.parentElement.parentElement.style.display="none"
     })
 
-    //
     var hamburguer = document.querySelector('.hamburguer-menu')
     hamburguer.addEventListener('click',function(){
         cross.parentElement.parentElement.style.display="block"
