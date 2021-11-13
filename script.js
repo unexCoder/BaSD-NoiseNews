2309159cf39ec0590a0cb3ca6f745b95f691c5a1
Luigi Tamagnini <bosca.music@gmail.com>
2021-11-13 09:07:12 -0300

    minor_changes_script.js

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
 window.onload = function() {
-
-    // modal mobile navbar 
-    var crosss = document.querySelectorAll('.cross-icon-modal')    
-    console.log(crosss)
-    for(let i=0; i<crosss.length; i++){
-        crosss[i].addEventListener("click", function(){
-            document.querySelector('#suscription-accepted').style.display="none";
-            document.querySelector('#suscription-fails').style.display="none";
-        });
-    }
             
     var cross = document.querySelector('.cross-icon');
     cross.addEventListener('click',function(){
